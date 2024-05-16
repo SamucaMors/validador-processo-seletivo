925c74340ff356189b45197463bd0f5eccf4f4ba
Wellington Moraes <samuca.mors@gmail.com>
2024-05-16 11:34:11 -0300

    2-Criando metodo estatico 'selecaoCandidatos'.

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,40 @@
 package candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("PROCESSO SELETIVO");
-        analisarCandidato(2000.20);
+        selecaoCandidatos();
     }
+
+    static void selecaoCandidatos(){
+        //Array com a lista de candidatos
+        String [] candidatos = {"SAMUEL", "RICARDO", "VICTOR", "MARIA", "FERNANDA", "INGRID", "MIGUEL", "NICOLE", "ROBERTA", "KAMILE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase=2000.0;
+
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário R$" + salarioPretendido);
+            if(salarioPretendido <= salarioBase){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+
+    }
+
+    //Método que simula o valor pretendido
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.00;
         if(salarioBase>salarioPretendido){
@@ -16,3 +47,4 @@ public class ProcessoSeletivo {
         }
     }
 }
+
