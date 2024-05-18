89f14185d34953eb117447371640708a7ba8d339
Wellington Moraes <samuca.mors@gmail.com>
2024-05-18 20:37:11 -0300

    3-Imprimindo uma lista com os candidatos selecionados

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,19 +1,31 @@
 package candidatura;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
+    static List<String> selecionados = new ArrayList<>();
+    static int candidatosSelecionados = 0;
     public static void main(String[] args) {
         System.out.println("PROCESSO SELETIVO");
         selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados(){
+        System.out.println("Imprimindo a lista de candidatos selecionados: ");
+        for(String candidato: selecionados){
+            System.out.println("Candidato selecionado: " + candidato);
+        }
     }
 
     static void selecaoCandidatos(){
         //Array com a lista de candidatos
         String [] candidatos = {"SAMUEL", "RICARDO", "VICTOR", "MARIA", "FERNANDA", "INGRID", "MIGUEL", "NICOLE", "ROBERTA", "KAMILE"};
 
-        int candidatosSelecionados = 0;
         int candidatoAtual = 0;
+
         double salarioBase=2000.0;
 
         while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
@@ -22,6 +34,7 @@ public class ProcessoSeletivo {
             System.out.println("O candidato " + candidato + " solicitou este valor de salário R$" + salarioPretendido);
             if(salarioPretendido <= salarioBase){
                 System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                selecionados.add(candidato);
                 candidatosSelecionados++;
             }
             candidatoAtual++;
